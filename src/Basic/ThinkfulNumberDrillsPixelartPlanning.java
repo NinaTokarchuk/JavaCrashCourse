99249d2649fb9b100656542ec7febc000f934109
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-08-27 11:48:24 +0300

    fourth task

--- src/Basic/ThinkfulNumberDrillsPixelartPlanning.java
+++ src/Basic/ThinkfulNumberDrillsPixelartPlanning.java
@@ -2,9 +2,9 @@ package Basic;
 
 public class ThinkfulNumberDrillsPixelartPlanning {
     public static void main(String[] args) {
-        System.out.println(isDivisible(2_000_000, 12345));
+        System.out.println(isDivisible(2_000_000, 0));
     }
     public static boolean isDivisible(int wallLength, int pixelSize) {
-        return wallLength % pixelSize == 0;
+        return pixelSize == 0 ? false : wallLength % pixelSize == 0;
     }
 }
