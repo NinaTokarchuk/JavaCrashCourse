c97760a13080c2f82450a8f17665e686e4a74901
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-08-27 11:11:26 +0300

    first task

---
+++ src/Basic/YouCantCodeUnderPressureNumber1.java
@@ -0,0 +1,12 @@
+package Basic;
+
+public class YouCantCodeUnderPressureNumber1 {
+    public static void main(String []args)
+    {
+        System.out.println(doubleInteger(3));
+    }
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i*2;
+    }
+}
