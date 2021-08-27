9a2168638278c859d5a818234d090ec4e4668304
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-08-27 11:20:16 +0300

    second task

---
+++ src/Basic/Function3MultiplyingTwoNumbers.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class Function3MultiplyingTwoNumbers {
+    public static void main(String []args) {
+        System.out.println(multiply(2,3));
+    }
+    public static int multiply(int num1, int num2) {
+        return num1*num2;
+    }
+}
