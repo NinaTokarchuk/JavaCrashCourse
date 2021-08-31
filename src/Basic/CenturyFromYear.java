3807c273bfe2572e99be369122f73548e38c8640
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-08-31 17:07:16 +0300

    eighth task

--- src/Basic/CenturyFromYear.java
+++ src/Basic/CenturyFromYear.java
@@ -2,11 +2,11 @@ package Basic;
 
 public class CenturyFromYear {
     public static void main(String[] args) {
-        System.out.println(century(10001));
+        System.out.println(century(150));
     }
     public static int century(int number) {
         int result = number / 100;
-        if(number % 10 > 0)
+        if(number % 100 > 0)
             result += 1;
         return result;
     }
