a71bc575fb47bf52c3431bec464698073b7acd5b
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-09-07 23:34:43 +0300

    10

---
+++ src/Conditions/WillThereBeEnoughSpace.java
@@ -0,0 +1,7 @@
+package Conditions;
+
+public class WillThereBeEnoughSpace {
+    public static int enough(int cap, int on, int wait) {
+        return (cap - on) >= wait ? 0 : wait - (cap - on);
+    }
+}
