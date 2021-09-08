159a2d4e0116667f68146f282813b4bf9972bfa4
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-09-08 11:23:32 +0300

    04

---
+++ src/Loops/BeginnerLostWithoutAMap.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class BeginnerLostWithoutAMap {
+    public static int[] map(int[] arr) {
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i] * 2;
+        }
+        return res;
+    }
+}
