a4578ee52ae1e01fa281d6f982992b180b808a2d
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-09-14 23:38:24 +0300

    15

---
+++ src/String/Correct.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Correct {
+    public static String correct(String string) {
+        string = string.replace("5", "S");
+        string = string.replace("0", "O");
+        string = string.replace("1", "I");
+        return string;
+    }
+}
