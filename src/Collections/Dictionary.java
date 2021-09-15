9da15202d1360c6e8991061f69b7f271e43218fb
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-09-15 12:51:36 +0300

    04

---
+++ src/Collections/Dictionary.java
@@ -0,0 +1,22 @@
+package Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dictionary {
+    private Map<String, String> dict;
+
+    public Dictionary() {
+        dict = new HashMap<>();
+    }
+
+    public void newEntry(String key, String value) {
+        dict.put(key, value);
+    }
+
+    public String look(String key) {
+        if (dict.containsKey(key))
+            return dict.get(key);
+        return "Cant find entry for " + key;
+    }
+}
