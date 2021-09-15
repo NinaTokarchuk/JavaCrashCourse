45f28d210a22f5fa7cae4f90cbe2d52a833ea6d9
Nina Tokarchuk <tokarchuknina@gmail.com>
2021-09-15 12:36:33 +0300

    02

---
+++ src/Collections/UnfinishedLoop.java
@@ -0,0 +1,13 @@
+package Collections;
+
+import java.util.*;
+
+public class UnfinishedLoop {
+    public static List CreateList(int number) {
+        List list = new ArrayList();
+        for (int count = 1; count <= number; count++) {
+            list.add(count);
+        }
+        return list;
+    }
+}
